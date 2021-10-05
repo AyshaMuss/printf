b42560b98a9e0e8a4368ffe7e86f0100d7b53e32
AyshaMuss <ayshamuss@gmail.com>
2021-10-05 15:29:32 +0300

    group project printf

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - fuction to to return write
- * @c: is a charater to the function
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: returns write.
-**/
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 return (write(1, &c, 1));
